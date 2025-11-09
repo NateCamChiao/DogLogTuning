269a5b208c9fd449df5baafe2b1ea0aac304fcac
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-11-08 21:00:30 -0800

    Adding another example

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,14 +12,13 @@ import frc.robot.TalonMotorTuner;
 
 public class Intake extends SubsystemBase{
     private TalonFX intakeMotor = new TalonFX(1);
+    private TalonFX armMotor = new TalonFX(1);
     
     public Intake(){
-
     }
 
     public void tuneMotors(){
-        if(DriverStation.isTest()){
-            TalonMotorTuner.tunablePID("Intake motor", intakeMotor, new Slot0Configs());
-        }
+        TalonMotorTuner.tunablePID("Intake motor", intakeMotor, new Slot0Configs());
+        TalonMotorTuner.tunablePIDFeedforward("Intake Arm Motor", armMotor, new Slot0Configs());
     }
 }
